feba2837ed40664c96acca1b42549cfa0b059880
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-03-14 13:30:29 +0700

    simple login page

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.loginTitle}>ĐĂNG NHẬP</Text>
+      <View style={styles.loginContainer}>
+        <Text>Email</Text>
+        <TextInput placeholder='Nhập email'/>
+        <Text>Mật khẩu</Text>
+        <TextInput placeholder='Nhập mật khẩu'/>
+      </View>
     </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    margin: 'auto',
+  },
+  loginContainer: {
+    backgroundColor: '#DBE2EC',
+    marginTop: 20,
+    padding: 20,
+  },
+  loginTitle: {
+    fontFamily: 'quicksand-bold',
+    fontSize: 24,
+    color: 'rgb(33,53,85)',
+  }
+})
