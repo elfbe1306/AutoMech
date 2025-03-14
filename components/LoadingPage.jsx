45905521e03c9b55eb0cd4ee74cedebb42858c72
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-03-14 18:32:35 +0700

    rotate gear

--- components/LoadingPage.jsx
+++ components/LoadingPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { Text, View, Image, StyleSheet } from "react-native";
+import React, { useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, Animated } from "react-native";
 
 export default function LoadingPage() {
+  const rotateValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotateValue, {
+        toValue: 1,
+        duration: 4000, // seconds for a full rotation
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [rotateValue]);
+
+  const rotate = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>AUTOMECH</Text>
-      <Image source={require('../assets/images/logo.png')}/>
+      <Animated.Image
+        source={require('../assets/images/logo.png')}
+        style={{transform: [{ rotate }] }}
+      />
     </View>
   );
 }
@@ -22,5 +42,6 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontFamily: "quicksand-bold",
     fontWeight: "bold",
+    color: "rgb(33,53,85)"
   }
 })
